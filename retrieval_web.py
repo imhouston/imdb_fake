4a10444b6c41a11325be8ac56baaf508121bb4ef
Pushkinue <teslenkoden@gmail.com>
2019-06-18 21:35:07 +0500

    Init server

--- retrieval_web.py
+++ retrieval_web.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, Response
-from jsonrpcserver import dispatch, method
-import requests
+from flask import Flask, request, jsonify
 
 SOLR_HOST = ""
 
 app = Flask(__name__)
 
 
-@method
-def query_search(query_text: str):
+def query_search(query_json: dict):
+    # query_string = "http://" + SOLR_HOST
+    # res = requests.get(query_string)
+    # query_result = res.json()
+    return {}
+
+
+def add_sponsor(sponsor_id: str):
     pass
     # query_string = "http://" + SOLR_HOST
     # res = requests.get(query_string)
     # query_result = res.json()
-    return
+    return {}
 
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    req = request.get_data().decode()
-    response = dispatch(req)
-    return Response(str(response), response.http_status, mimetype="application/json")
+@app.route("/search", methods=['POST'])
+def search_api():
+    req = request.get_json()
+    search_results = query_search(req)
+
+    return jsonify(search_results)
+
+
+@app.route("/sponsor", methods=['GET'])
+def sponsor_api():
+    if request.args.get('id'):
+        sponsor_id = request.args.get('id')
+    sponsor_status = add_sponsor(sponsor_id)
+    return jsonify(sponsor_id)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5002, debug=True)
